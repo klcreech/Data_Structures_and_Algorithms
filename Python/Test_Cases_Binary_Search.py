83d26ff8cbeff7a5b396d8b17e970dee684691b0
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2023-04-18 14:40:37 +0200

    Update Test_Cases_Binary_Search.py

--- Python/Test_Cases_Binary_Search.py
+++ Python/Test_Cases_Binary_Search.py
@@ -1,14 +1,13 @@
 # Lesson 1 - Binary Search, Linked Lists and Complexity
 # the following is a test function for the binary search used to test various queries
 
-# Problem:
-
-# Alice has some cards with numbers written on them. She arranges the cards in decreasing
-# order, and lays them out face down in a sequence on a table. She challenges Bob to pick
-# out the card containing a given number by turning over as few cards as possible. Write
-# a function to help Bob locate the card.
 
 
+# Step 1
+
+# We need to write a program to find the position of a given number in a list
+# of numbers arranged in decreasing order. We also need to minimize the number
+# of times we access elements from the list. Identify input and output formats.
 
 
 
