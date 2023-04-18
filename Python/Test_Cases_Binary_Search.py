5cb722f5e1b1de3d3082ddac782cd9bc6a4ca0a4
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2023-04-18 14:41:50 +0200

    Update Test_Cases_Binary_Search.py

--- Python/Test_Cases_Binary_Search.py
+++ Python/Test_Cases_Binary_Search.py
@@ -16,6 +16,94 @@
 
 
 
+import timeit
+
+# Test Function ( Step 1 )
+
+def locate_card(cards, query):
+     # Create a variable position with the value 0
+    position = 0
+    # Set up a loop for repetition that also test empty array
+    while position < len(cards):
+        # Check if element at the current position matches the query
+        if cards[position] == query:
+            # Answer found! Return and exit..
+            return position
+        # Increment the position
+        position += 1
+    # Number not found, return -1
+    return -1
+
+# Function (linear search) ( Step 4 )
+def locate_card_linear(cards, query):
+    position = 0
+    while position < len(cards):
+        if cards[position] == query:
+            return position
+        position += 1
+    return -1
+
+# Function (binary search) ( Step 7 )
+def locate_card_binary(cards, query):
+    lo, hi = 0, len(cards) - 1
+
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if cards[mid] == query:
+            return mid
+        elif cards[mid] < query:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+
+    return -1
+
+# Step 2 
+
+# Come up with some example inputs & outputs. Try to cover all edge cases.
+
+
+tests = [
+    # base test
+   {
+    'input': { 
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0], 
+        'query': 7
+    },
+    'output': 3
+    },
+     # query occurs in the middle
+    {
+        'input': {
+            'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+            'query': 1
+    },
+    'output': 6
+    },
+    # query is the first element
+    {
+        'input': {
+            'cards': [4, 2, 1, -1],
+            'query': 4
+        },
+        'output': 0
+    },
+    # query is the last element
+    {
+        'input': {
+            'cards': [3, -1, -9, -127],
+            'query': -127
+        },
+        'output': 3
+    },
+     # cards contains just one element
+    {
+        'input': {
+            'cards': [6],
+            'query': 6
+        },
+        'output': 0
+    },
 
     # Step 3
     # issue with test 6
