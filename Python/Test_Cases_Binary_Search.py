0823f00f89d0c457988a3923a953269c1a89aba5
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2023-04-20 20:33:40 +0200

    Update Test_Cases_Binary_Search.py

--- Python/Test_Cases_Binary_Search.py
+++ Python/Test_Cases_Binary_Search.py
@@ -8,6 +8,16 @@
 # out the card containing a given number by turning over as few cards as possible. Write
 # a function to help Bob locate the card.
 
+
+# - The Method
+
+# 1. State the problem clearly. Identify the input & output formats.
+# 2. Come up with some example inputs & outputs. Try to cover all edge cases.
+# 3. Come up with a correct solution for the problem. State it in plain English.
+# 4. Implement the solution and test it using example inputs. Fix bugs, if any.
+# 5. Analyze the algorithm's complexity and identify inefficiencies, if any.
+# 6. Apply the right technique to overcome the inefficiency. Repeat steps 3 to 6.
+
 # Step 1
 
 # We need to write a program to find the position of a given number in a list
