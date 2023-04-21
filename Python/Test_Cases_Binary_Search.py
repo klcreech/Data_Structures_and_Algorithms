7a990f85056140ee8d8cd1030ef549c5de0b9e8b
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2023-04-21 17:10:52 +0200

    Update Test_Cases_Binary_Search.py

--- Python/Test_Cases_Binary_Search.py
+++ Python/Test_Cases_Binary_Search.py
@@ -91,7 +91,7 @@ tests = [
     # base test
    {
     'input': { 
-        'cards': [13, 11, 10, 7, 4, 3, 1, ], 
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0], 
         'query': 7
     },
     'output': 3
@@ -99,7 +99,7 @@ tests = [
      # query occurs in the middle
     {
         'input': {
-            'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+            'cards': [13, 11, 10, 7, 4, 3, 1, ],
             'query': 1
     },
     'output': 6
