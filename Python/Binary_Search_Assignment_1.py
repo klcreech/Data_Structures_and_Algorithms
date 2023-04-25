c84bee073d16431a49d480d98e046c6455db61dd
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2023-04-25 17:35:25 +0200

    Update Binary_Search_Assignment_1.py
    Include Problem and Method

--- Python/Binary_Search_Assignment_1.py
+++ Python/Binary_Search_Assignment_1.py
@@ -1,1 +1,26 @@
 # Jovian Course - Data Structures and Algorithms - Binary Search Assignment 1
+
+
+# Problem
+
+# You are given list of numbers, obtained by rotating a sorted list an unknown number of times. 
+# Write a function to determine the minimum number of times the original sorted list was rotated
+# to obtain the given list. Your function should have the worst-case complexity of O(log N), 
+# where N is the length of the list. You can assume that all the numbers in the list are unique.
+
+   # Example: The list [5, 6, 9, 0, 2, 3, 4] was obtained by rotating the sorted list [0, 2, 3, 4, 5, 6, 9] 3 times.
+
+# We define "rotating a list" as removing the last element of the list and adding it before the
+#  first element. E.g. rotating the list [3, 2, 4, 1] produces [1, 3, 2, 4].
+
+   # "Sorted list" refers to a list where the elements are arranged in the increasing order e.g. [1, 3, 5, 7].
+
+# Method
+
+# 1. State the problem clearly. Identify the input & output formats.
+# 2. Come up with some example inputs & outputs. Try to cover all edge cases.
+# 3. Come up with a correct solution for the problem. State it in plain English.
+# 4. Implement the solution and test it using example inputs. Fix bugs, if any.
+# 5. Analyze the algorithm's complexity and identify inefficiencies, if any.
+# 6. Apply the right technique to overcome the inefficiency. Repeat steps 3 to 6.
+
