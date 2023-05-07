688465904c8c576bebf09958329199c5db3f5f08
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2023-05-07 20:50:58 +0200

    Update Binary_Search_Assignment_1.py

--- Python/Binary_Search_Assignment_1.py
+++ Python/Binary_Search_Assignment_1.py
@@ -248,6 +248,8 @@ for i, test in enumerate(tests):
   
    # Step 4 ( Binary Search )
 
+   
+
    # Implement the solution and test it using example inputs. Fix bugs, if any.
    # Binary Search Placed under linear function all test cases pass
 
